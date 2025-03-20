3168b36ee4f842c4c1ab162ead6db54d51462cf7
bonjour1000 <clement.jacquet@epita.fr>
2025-03-20 17:17:59 +0100

    feat: add login form

--- app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
+++ app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
@@ -3,21 +3,15 @@ package dev.xdbe.booking.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -28,10 +22,10 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(auth -> auth
-                // Step 3: add authorization
-                .anyRequest().permitAll()
+                .requestMatchers("/dashboard").hasRole("ADMIN")  // Only ADMIN can access dashboard
+                .anyRequest().permitAll()                        // All other URLs are allowed for everyone
             )
-            // Step 3: Add login form
+            .formLogin(withDefaults())
             .csrf((csrf) -> csrf
                 .ignoringRequestMatchers("/h2-console/*")
             )
@@ -39,6 +33,25 @@ public class SecurityConfiguration {
             .build();
     }
 
-    // Step 3: add InMemoryUserDetailsManager
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password("$2a$10$Wtm9hMaSXRJv09SrYFBbWu8WdYECpKfpSAqycc0wecOvFfhN1.OHa")
+            .roles("ADMIN")
+            .build();
+        
+        UserDetails user = User.builder()
+            .username("user")
+            .password("$2a$10$M3StXV3ZjonHj6BxTL0SYuHxJwlNweb2tvagrCcrlKNaL1fyGbOVa")
+            .roles("USER")
+            .build();
 
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
